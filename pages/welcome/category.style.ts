d2daebae863eee23c7219b8c7de99532cbd9bb8d
maxf_fmr <maxime.kerlidou@gmail.com>
2022-07-28 18:57:01 +0200

    parralax effect fixed

--- pages/welcome/category.style.ts
+++ pages/welcome/category.style.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 interface props {
   bgUrl: string;
 }
-const StyledWelcome = styled.section<props>`
+const StyledWelcome = styled.section`
   .block {
     height: 350px;
     width: 100vw;
     overflow: hidden;
+    position: relative;
 
     img {
       object-fit: cover;
@@ -15,3 +16,10 @@ const StyledWelcome = styled.section<props>`
 `;
 
 export default StyledWelcome;
+
+export const StyledItem = styled.div<props>`
+  border: solid black 3px;
+  background-image: url(${(props) => props.bgUrl});
+  height: 350px;
+  background-size: cover;
+`;
--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -1,43 +1,36 @@
 import { Parallax } from 'react-scroll-parallax';
 import categories from './categories';
-import StyledWelcome from './category.style';
+import StyledWelcome, { StyledItem } from './category.style';
 
 const Welcome = () => {
   return (
-    <>
-      <StyledWelcome bgUrl={categories[0].url} key={categories[0].id}>
-        <div className='block'>
-          <Parallax speed={-100}>
-            <img src={categories[0].url} alt='' />{' '}
-          </Parallax>
-        </div>
-        <div className='block'>
-          <Parallax speed={-60}>
-            <img src={categories[1].url} alt='' />{' '}
-          </Parallax>
-        </div>
-        <div className='block'>
-          <Parallax speed={-70}>
-            <img src={categories[2].url} alt='' />{' '}
-          </Parallax>
-        </div>
-        <div className='block'>
-          <Parallax speed={-50}>
-            <img src={categories[3].url} alt='' />{' '}
-          </Parallax>
-        </div>
-        <div className='block'>
-          <Parallax speed={-50}>
-            <img src={categories[4].url} alt='' />{' '}
-          </Parallax>
-        </div>
-        <div className='block'>
-          <Parallax speed={-50}>
-            <video src={categories[5].url} loop autoPlay muted />{' '}
-          </Parallax>
-        </div>
-      </StyledWelcome>
-    </>
+    <StyledWelcome>
+      <div className='block'>
+        <Parallax speed={-30} startScroll={0} endScroll={900}>
+          <img src={categories[0].url} alt='' />{' '}
+        </Parallax>
+      </div>
+      <div className='block'>
+        <Parallax speed={-30} startScroll={0} endScroll={900}>
+          <img src={categories[1].url} alt='' />{' '}
+        </Parallax>
+      </div>
+      <div className='block'>
+        <Parallax speed={-70}>
+          <img src={categories[2].url} alt='' />{' '}
+        </Parallax>
+      </div>
+      <div className='block'>
+        <Parallax speed={-50}>
+          <img src={categories[3].url} alt='' />{' '}
+        </Parallax>
+      </div>
+      <div className='block'>
+        <Parallax speed={-50}>
+          <video src={categories[5].url} loop autoPlay muted />{' '}
+        </Parallax>
+      </div>
+    </StyledWelcome>
   );
 };
 export default Welcome;
