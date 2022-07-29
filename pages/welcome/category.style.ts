01f9ec23db012012b643f80728e681799a874765
maxf_fmr <maxime.kerlidou@gmail.com>
2022-07-29 19:14:21 +0200

    video item itegrations

--- pages/welcome/category.style.ts
+++ pages/welcome/category.style.ts
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
 
-interface Props {
-  categoryId: number;
-  bgUrl: string;
-}
-const StyledWelcome = styled.section<Props>`
+const StyledWelcome = styled.section`
+  background-color: #3f3f3f;
   width: 100%;
   .container1 {
     height: 300px;
+
+    overflow: hidden;
+    @media (max-width: 700px) {
+      height: 150px;
+    }
+  }
+  .videoItem {
+    display: flex;
+    height: 300px;
+    overflow: hidden;
+    justify-content: center;
+    @media (max-width: 700px) {
+      height: 150px;
+    }
+    video {
+      object-fit: cover;
+      width: 100%;
+    }
   }
 `;
 
--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -5,15 +5,20 @@ import StyledWelcome from './category.style';
 const Welcome = () => {
   return (
     <>
-      {categories.map((category) => {
-        return (
-          <StyledWelcome bgUrl={category.url} categoryId={category.id}>
-            <ParallaxBanner layers={[{ image: category.url, speed: -40 }]}>
-              <div className='container1'></div>
-            </ParallaxBanner>
-          </StyledWelcome>
-        );
-      })}
+      <StyledWelcome>
+        {categories.map((category) => {
+          if (category.theme !== 'video') {
+            return (
+              <ParallaxBanner layers={[{ image: category.url, speed: -40 }]}>
+                <div className='container1'></div>
+              </ParallaxBanner>
+            );
+          }
+        })}
+        <div className='videoItem'>
+          <video src={categories[5].url} autoPlay loop muted></video>
+        </div>
+      </StyledWelcome>
     </>
   );
 };
