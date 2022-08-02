a56061a9b42eebb6c9b65db62606d31451a4f3e2
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2022-08-02 11:33:58 +0200

    carroussel setup

--- pages/weeding/index.tsx
+++ pages/weeding/index.tsx
@@ -1,6 +1,29 @@
 import Header from '../../components/Header';
 import StyledWeedingPage from './weeding.style';
+import ImageGallery from 'react-image-gallery';
 import Image from 'next/image';
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+const images = [
+  {
+    original:
+      '  https://res.cloudinary.com/dal6ewwdl/image/upload/w_4000,h_4000,c_limit/Cl%C3%A9ment%20Bayard%20Website/wedding/wedding_vvf6tc.jpg',
+    thumbnail:
+      ' https://res.cloudinary.com/dal6ewwdl/image/upload/w_4000,h_4000,c_limit/Cl%C3%A9ment%20Bayard%20Website/wedding/wedding_vvf6tc.jpg',
+  },
+  {
+    original:
+      'https://res.cloudinary.com/dal6ewwdl/image/upload/w_4000,h_4000,c_limit/Cl%C3%A9ment%20Bayard%20Website/wedding/DSC08191_u2x1lp.jpg',
+    thumbnail:
+      'https://res.cloudinary.com/dal6ewwdl/image/upload/w_4000,h_4000,c_limit/Cl%C3%A9ment%20Bayard%20Website/wedding/DSC08191_u2x1lp.jpg',
+  },
+  {
+    original:
+      'https://res.cloudinary.com/dal6ewwdl/image/upload/w_4000,h_4000,c_limit/Cl%C3%A9ment%20Bayard%20Website/wedding/DSC08532_enzvnh.jpg',
+    thumbnail:
+      'https://res.cloudinary.com/dal6ewwdl/image/upload/w_4000,h_4000,c_limit/Cl%C3%A9ment%20Bayard%20Website/wedding/DSC08532_enzvnh.jpg',
+  },
+];
 
 const WeedingPage = (): JSX.Element => {
   return (
@@ -33,6 +56,7 @@ const WeedingPage = (): JSX.Element => {
             />
           </div>
         </div>
+        <ImageGallery items={images} />
       </StyledWeedingPage>
     </>
   );
