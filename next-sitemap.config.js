82f3292ac9f539738a0f3bce187895adaf6b5e81
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2022-09-22 13:46:25 +0200

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+const config = {
+  siteUrl: 'https://www.bayardclement.fr',
+  generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+  // Other available options..
+
+  exclude: [
+    '/blank',
+    '/blog',
+    '/contact/contact.style',
+    '/food/food.style',
+    '/infos/infos.style',
+    '/videos/videos.style',
+    '/wedding/weeding.style',
+    '/welcome/category.style',
+    '/index.style',
+  ],
+};
+
+module.exports = config;
--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
 module.exports = nextConfig;
 
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-    ];
-  },
   images: {
     domains: [
       'res.cloudinary.com',
